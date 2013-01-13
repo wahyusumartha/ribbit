9f396c49e1e1cd15a6551e6a4aee3b2bfe7ec939
Wahyu Sumartha <wahyusumartha@gmail.com>
2013-01-13 16:13:13 +0800

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ActiveRecord::Base
+
+  before_validation :prep_email
+  before_save :create_avatar_hash
+
+  attr_accessible :avatarurl, :email, :name, :password, :password_confirmation, :username
+
+  has_secure_password
+
+  validates :name, :presence => true
+  validates :username, :presence => true, :uniqueness => true 
+  validates :email, :presence => true, :uniqueness => true, :format => { with: /^[\w\.+-]+@([\w]+\.)+\w+$/ }
+
+  private 
+
+  def prep_email 
+  	self.email = self.email.downcase if self.email
+  end
+
+  def create_avatar_hash
+  	self.avatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=50"
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 RibbitApp::Application.routes.draw do
-  get "home/index"
+  resources :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
